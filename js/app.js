b5a1882fff72599a90185fa07f9ccf731f47fc46
Natalia <kns.natalia@gmail.com>
2018-06-22 11:56:11 +0200

    fixes

--- js/app.js
+++ js/app.js
@@ -1,73 +1,106 @@
+class Game {
+	static get gestures() {
+		return {
+			'Scissors': 'Paper',
+			'Rock': 'Scissors',
+			'Paper': 'Rock'
+		};
+	}
+
+	bindEvents() {
+		this.container.querySelector('.btns-wrapper').addEventListener('click', (e) => {
+			if (!e.target.matches('button')) {
+				return;
+			}
+			this.player.gesture = e.target.textContent;
+			this.duel.execute();
+		});
+	}
+
+	constructor(container) {
+		this.container = document.querySelector(container);
+		this.player = new Player();
+		this.computer = new Computer();
+		this.logWindow = new LogWindow( this.container.querySelector('.log-window') );
+		this.duel = new Duel(this.player, this.computer, this.logWindow);
+		this.bindEvents();
+	}
+}
+
+class Player {
+	getGesture() {
+		const gesture = this.gesture;
+		this.gesture = null;
+		return gesture;
+	}
+}
+
+class Computer {
+	getGesture() {
+		const gestures = Object.keys(Game.gestures);
+		return gestures[Math.floor(Math.random() * gestures.length)];
+	}
+}
+
 class Duel {
-  constructor() {
-    this.gesture = [
-      "Paper",
-      "Rock",
-      "Scissors"
-    ];
-    this.btns = document.querySelectorAll('.btn');
-  }
-  randomGest() {
-    this.randomItem = this.gesture[Math.floor(Math.random()*this.gesture.length)];
-    return this.randomItem;
-  }
-  returnBtnText(btnText) {
-    return btnText;
-  }
-  onClickFn() {
-    this.btns.forEach((el) => {
-      el.addEventListener('click', () => {
-        this.returnBtnText(el.innerHTML);
-        this.randomGest();
-        this.logWindow = document.querySelector('.log-window');
-        if(this.randomItem === el.innerHTML) {
-          this.logWindow.innerHTML+=    `<div>User: ${el.innerHTML}, PC: ${this.randomItem}. Draw</div>`;
-        } else if (this.randomItem ===      'Paper' && el.innerHTML === 'Rock') {
-          this.logWindow.innerHTML+=    `<div>User: ${el.innerHTML}, PC: ${this.randomItem}. PC wins</div>`;
-        } else if (this.randomItem ===      'Rock' && el.innerHTML === 'Scissors') {
-          this.logWindow.innerHTML+=    `<div>User: ${el.innerHTML}, PC: ${this.randomItem}. PC wins</div>`;
-        } else if (this.randomItem ===      'Scissors' && el.innerHTML === 'Paper') {
-          this.logWindow.innerHTML+=    `<div>User: ${el.innerHTML}, PC: ${this.randomItem}. PC wins</div>`;
-        } else if (this.randomItem ===      'Rock' && el.innerHTML === 'Paper') {
-          this.logWindow.innerHTML+=    `<div>User: ${el.innerHTML}, PC: ${this.randomItem}. User wins</div>`;
-        } else if (this.randomItem ===      'Scissors' && el.innerHTML === 'Rock') {
-          this.logWindow.innerHTML+=    `<div>User: ${el.innerHTML}, PC: ${this.randomItem}. User wins</div>`;
-        } else if (this.randomItem ===      'Paper' && el.innerHTML === 'Scissors') {
-          this.logWindow.innerHTML+=    `<div>User: ${el.innerHTML}, PC: ${this.randomItem}. User wins</div>`;
-        } else {
-          this.logWindow.innerHTML+=    `<div>Something went wrong. Try again</div>`
-        }
-      });
-    });
-  }
+	constructor(player, computer, logWindow) {
+		Object.assign(this, {player, computer, logWindow});
+	}
+
+	getWinner(playerGesture, computerGesture) {
+		if (Game.gestures[playerGesture] === computerGesture) {
+			return 'User';
+		} else if (Game.gestures[computerGesture] === playerGesture) {
+			return 'PC';
+		}
+		return 'Draw';
+	}
+
+	execute() {
+		const playerGesture = this.player.getGesture();
+		const computerGesture = this.computer.getGesture();
+		const winner = this.getWinner(playerGesture, computerGesture);
+		this.logWindow.log(playerGesture, computerGesture, winner);
+		this.logWindow.detectHeight();
+		this.logWindow.clearWindow();
+	}
 }
 
-class LogWindow extends Duel {
-  constructor() {
-    super();
-  }
-  detectHeight() {
-    this.logWindow = document.querySelector('.log-window');
-    this.divHeight = this.logWindow.clientHeight;
-    this.logsDivs = document.querySelectorAll('.log-window div');
-    return this.divHeight;
-  }
-  clearWindow() {
-    this.btns.forEach((el) => {
-      el.addEventListener('click', () => {
-        this.detectHeight();
-        if(this.divHeight > 344) {
-          while (this.logWindow.firstChild) {
-            this.logWindow.removeChild(this.logWindow.firstChild);
-          }
-          this.logWindow.innerHTML= `Log window was cleared`;
-        }
-      });
-    });
-  }
+class LogWindow {
+	constructor(logElement) {
+		this.logElement = logElement;
+	}
+
+	log(playerGesture, computerGesture, winner) {
+		this.li = document.createElement('li');
+		this.li.textContent = `User: ${playerGesture}, PC: ${computerGesture}.`;
+		if(winner !== 'Draw') {
+			this.li.textContent += ` ${winner} wins.`;
+		}
+		else {
+			this.li.textContent += ` Draw.`;
+		}
+		this.logElement.appendChild(this.li);
+	}
+
+	detectHeight() {
+		this.divHeight = this.logElement.clientHeight;
+		return this.divHeight;
+	}
+
+	clearWindow() {
+		const logLi = document.querySelectorAll('.log-window li');
+		this.detectHeight();
+		if(this.divHeight > 344) {
+			while (this.logElement.firstChild) {
+				this.logElement.removeChild(this.logElement.firstChild);
+			}
+			const clearedInfo = document.createElement('li');
+			clearedInfo.textContent = `Log window was cleared`;
+			this.logElement.appendChild(clearedInfo);
+			this.logElement.appendChild(this.li);
+		}
+	}
 }
 
-const duel = new Duel();
-const logWindowHeight = new LogWindow();
-duel.onClickFn();
-logWindowHeight.clearWindow();
+new Game('#game');
